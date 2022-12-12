f103fdd6018c67d756ef44f34714af3c2914eef3
activehubb <activehubb.gmail.com>
2022-12-12 11:15:52 +0100

    develop branch

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signUp } = require("../controller/userControler");
-const router = express.Router();
-
-// Admin || User
-router.route("/create").post(signUp);
-
-// Admin
-module.exports = router;
